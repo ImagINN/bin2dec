64b0b9ad5c86b507c940d1f053b499f2bdb33d95
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-04-09 21:39:18 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-\n");
+
+System.Console.WriteLine("Welcome to the Binary to Decimal Converter!");
+
+System.Console.WriteLine("\n-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-");
